6db879186f367e9f62a43413ed6faf4788a774d7
shishir bhandari <shishirbhaandari.public@gmail.com>
2026-04-15 12:16:15 +0545

    learned to display greater between two number

---
+++ greater.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if (a > b):
+    print("A is greater than B")
+elif (a < b):
+    print("B is greater than A")
+else:
+    print("Both numbers are equal")
