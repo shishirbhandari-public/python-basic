d39091016da0c8ba0ad6034528f8bd5f98e466b1
shishir bhandari <shishirbhaandari.public@gmail.com>
2026-04-22 10:45:33 +0545

    check the palindrome

---
+++ variable.py
@@ -0,0 +1,11 @@
+n=input("enter a string  :")
+a=""
+c=len(n)
+for i in range (c , 0 , -1):
+    a=a+n[i-1]
+print(a)
+if n==a:
+    print("it is a palindrome")
+else:
+    print("it is not a palindrome")
+    
