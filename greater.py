3aeada8bed5cb9043de46fe3efec7bc305f41320
Shishir <shishirbhandari.public@gmail.com>
2026-04-22 10:53:25 +0545

    to check greater between two number
    Updated input prompts for better formatting.

--- greater.py
+++ greater.py
@@ -1,5 +1,5 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
 
 if (a > b):
     print(f"{a} is greater than {b}")
